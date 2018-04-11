611e48aaff380b8d23aab7bd8a0c975c1abbd05c
Edith Hofer <edith.hofer@student.tugraz.at>
2018-04-11 10:43:03 +0200

    [EH,DA,JT] initial backend project, created first test

---
+++ backend/src/test/java/at/tugraz/recipro/RecipeTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.tugraz.recipro;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Edith
+ */
+public class RecipeTest {
+    
+    public RecipeTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+    @Test
+    public void createAndFindRecipe(){
+        String title = "grandmas best cake";
+        Recipe recipe = new Recipe();
+        recipe.setTitle(title);
+        recipeManager.createRecipe(recipe);
+        Recipe result = recipeManager.findByTitle(title);
+        assertThat(result.getTitle(), is(recipe.getTitle()));
+    }
+}
