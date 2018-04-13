9a7ed39b4e8b19cc0fd9145bd282398ceff31a56
Jonil Kodra <jonil.kodra@student.tugraz.at>
2018-04-13 15:49:07 +0200

    [CH,JK] Added Perform Search within Same Activity

--- app/app/src/androidTest/java/at/tugraz/recipro/recipro/SearchTest.java
+++ app/app/src/androidTest/java/at/tugraz/recipro/recipro/SearchTest.java
@@ -1,18 +1,31 @@
 package at.tugraz.recipro.recipro;
 
 import android.content.Context;
+import android.os.IBinder;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Root;
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.KeyEvent;
+import android.view.WindowManager;
 
+import org.hamcrest.Description;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsNot;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.pressKey;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.RootMatchers.withDecorView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(AndroidJUnit4.class)
 public class SearchTest {
@@ -21,7 +34,38 @@ public class SearchTest {
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
-    public void searchByName() {
+    public void searchBarAvailable() {
         onView(withHint(R.string.search_hint)).perform(click());
+    }
+
+    @Test
+    public void searchSubmitSearch() {
+        onView(withHint(R.string.search_hint)).perform(click());
+        onView(withHint(R.string.search_hint)).perform(ViewActions.typeTextIntoFocusedView("Test"), pressKey(KeyEvent.KEYCODE_ENTER));
+
+        onView(withText("Query: Test")).inRoot(new ToastMatcher())
+                .check(matches(isDisplayed()));
+    }
+
+    public class ToastMatcher extends TypeSafeMatcher<Root> {
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("is toast");
+        }
+
+        @Override
+        public boolean matchesSafely(Root root) {
+            int type = root.getWindowLayoutParams().get().type;
+            if ((type == WindowManager.LayoutParams.TYPE_TOAST)) {
+                IBinder windowToken = root.getDecorView().getWindowToken();
+                IBinder appToken = root.getDecorView().getApplicationWindowToken();
+                if (windowToken == appToken) {
+                    //means this window isn't contained by any other windows.
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
--- app/app/src/main/java/at/tugraz/recipro/recipro/MainActivity.java
+++ app/app/src/main/java/at/tugraz/recipro/recipro/MainActivity.java
@@ -5,10 +5,13 @@ import android.app.SearchManager;
 import android.app.SearchableInfo;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.CursorAdapter;
+import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.Toast;
 
@@ -19,11 +22,23 @@ public class MainActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SearchView searchBar = findViewById(R.id.searchbar);
+        final SearchView searchBar = findViewById(R.id.searchbar);
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         searchBar.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchBar.setSubmitButtonEnabled(true);
         searchBar.setIconifiedByDefault(false);
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                searchFor(s);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return true;
+            }
+        });
 
         // Get the intent, verify the action and get the query
         Intent intent = getIntent();
@@ -35,6 +50,6 @@ public class MainActivity extends ListActivity {
     }
 
     private void searchFor(String query) {
-        Toast.makeText(this, query, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Query: " + query, Toast.LENGTH_SHORT).show();
     }
 }
