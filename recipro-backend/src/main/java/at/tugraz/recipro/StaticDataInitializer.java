c6435b238f6961d060abab7096b736f4fb68f4dd
Julian Thonhauser <j.thonhauser@student.tugraz.at>
2018-05-04 14:15:19 +0200

    [CH,JT,RH] added basic implementation for allergens in backend

---
+++ recipro-backend/src/main/java/at/tugraz/recipro/StaticDataInitializer.java
@@ -0,0 +1,44 @@
+package at.tugraz.recipro;
+
+import at.tugraz.recipro.recipes.entity.Allergen;
+import java.util.Arrays;
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author julian
+ */
+@Startup
+@Singleton
+public class StaticDataInitializer {
+    @PersistenceContext
+    private EntityManager em;
+    
+    @PostConstruct
+    public void init() {
+        initAllergens();
+    }
+    
+    private void initAllergens() {
+        Allergen allergens[] = {new Allergen("P", "Peanuts"),
+                                new Allergen("N", "Nuts"),
+                                new Allergen("Mi", "Mulk"),
+                                new Allergen("So", "Soya"),
+                                new Allergen("Mu", "Mustard"),
+                                new Allergen("L", "Lupin"),
+                                new Allergen("E", "Eggs"),
+                                new Allergen("F", "Fish"),
+                                new Allergen("Sh", "Shellfish"),
+                                new Allergen("Mo", "Molluscs"),
+                                new Allergen("G", "Cerials containing gluten"),
+                                new Allergen("Se", "Sesame"),
+                                new Allergen("C", "Celery"),
+                                new Allergen("Su", "Sulphur dioxide")};
+        Arrays.stream(allergens).forEach((Allergen a) -> em.persist(a));
+    }
+}
---
+++ recipro-backend/src/main/java/at/tugraz/recipro/recipes/boundary/AllergensResource.java
@@ -0,0 +1,51 @@
+package at.tugraz.recipro.recipes.boundary;
+
+import at.tugraz.recipro.recipes.control.AllergensManager;
+import at.tugraz.recipro.recipes.entity.Allergen;
+import io.swagger.annotations.Api;
+import java.net.URI;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+/**
+ *
+ * @author Julian
+ */
+@Path("allergens")
+@Stateless
+@Api(value = "RecipesResource")
+public class AllergensResource {   
+    @Inject
+    AllergensManager allergensManager;
+    
+    @POST
+    public Response create(Allergen allergen, @Context UriInfo uriInfo) {
+        Allergen savedAllergen = allergensManager.save(allergen);
+        String shortName = savedAllergen.getShortName();
+        URI uri = uriInfo.getAbsolutePathBuilder().path("/" + shortName).build();
+        return Response.created(uri).build();
+    }
+    
+    @GET
+    @Path("{shortName}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Allergen findByShortName(@PathParam("shortName") String shortName) {
+        return allergensManager.findByShortName(shortName);
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Allergen> findAll() {
+        return allergensManager.findAll();
+    }
+}
